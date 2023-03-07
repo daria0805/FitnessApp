e991632e51a65b930b5e0451f7437d017f53f0e7
daria0805 <dariabobitan@yahoo.com>
2023-03-07 20:54:11 +0200

    commit installing2

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
     private array $messages = [
         "Hello", "Hi", "Bye!"
@@ -17,11 +18,18 @@ class HelloController
     {
         return new Response(implode(',', $this->messages));
         return new Response(implode(',', array_slice($this->messages, 0, $limit)));
+        return $this->render('hello/index.html.twig', ['message' => implode(',', array_slice($this->messages, 0, $limit))]);
     }
 
     #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
     public function showOne($id): Response
     {
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+            'message' => $this->messages[$id]
+            ]
+        );
         return new Response($this->messages[$id]);
     }
 }
